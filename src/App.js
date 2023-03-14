13057e643a4f876e410e51ce733be814aa996c47
Kahmedowa <k.ahmedowa94@gmail.com>
2023-03-14 16:05:48 +0100

    commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Home from './component/Home';
+import Navbar from './component/Navbar';
+import Products from './component/Products';
+
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    <Products/>
+    </>
+
+  );
+}
+
+export default App;
